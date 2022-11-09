285170be921cea06a5bb95bcb8b97d5fa673535e
shingo1013 <117624040+shingo1013@users.noreply.github.com>
2022-11-09 11:34:10 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,121 @@
+$(function () {
+  /*-----------------------------------
+  ハンバーガーメニュー
+  ------------------------------------*/
+  $('.hamburger').click(function () {
+    hamburger();
+  });
+
+  $('#navi a').click(function () {
+    hamburger();
+  });
+  /*------------------------------------
+  スムーススクロール
+  --------------------------------------*/
+  $('a[href^="#"]').click(function () {
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $('body,html').animate({ scrollTop: position }, 1000, 'swing');
+    return false;
+  });
+  /*-------------------------------------
+  fade in
+  --------------------------------------*/
+  $('.inview').on('inview', function (event, isInView) {
+    if (isInView) {
+      $(this).stop().addClass('show');
+    }
+  });
+  /*----------------------------------------
+  スクロールイベント
+  -----------------------------------------*/
+  $(window).scroll(function () {
+    let scroll = $(window).scrollTop();
+    /*------------------------------------
+    mainvisual
+    -------------------------------------*/
+    mv_scale(scroll);
+    /*------------------------------------
+    logo hamburger
+    -------------------------------------*/
+    if (scroll > 520) {
+      $('.logo').fadeIn(500);
+      $('.hamburger').fadeIn(500);
+    } else {
+      $('.logo').fadeOut(500);
+      $('.hamburger').fadeOut(500);
+    }
+    /*------------------------------------
+    side-btn
+    ---------------------------------------*/
+    let gallery_position = $('#gallery').offset().top - $(window).height();
+    let access_position = $('#access').offset().top - $(window).height();
+    if (window.innerWidth > 900) {
+      if (scroll > gallery_position) {
+        if (scroll < access_position) {
+          $('#side-btn').css({
+            'transform': 'rotate(-90deg) translateY(0)'
+          });
+        } else {
+          $('#side-btn').css({
+            'transform': 'rotate(-90deg) translateY(60px)'
+          });
+        }
+      } else {
+        $('#side-btn').css({
+          'transform': 'rotate(-90deg) translateY(60px)'
+        });
+      }
+    }
+    /*--------------------------------------
+    accessの背景
+    ----------------------------------------*/
+    let contact_position = $('#contact').offset().top - $(window).height();
+    if (scroll > access_position) {
+      if (scroll < contact_position) {
+        $('.bg').fadeIn(500);
+      } else {
+        $('.bg').fadeOut(500);
+      }
+    } else {
+      $('.bg').fadeOut(500);
+    }
+  });
+
+  /*--------------------------------------
+  画面読み込み時と画面幅変更時のイベント
+  ---------------------------------------*/
+  $(window).on('load resize', function () {
+    let scroll = $(window).scrollTop();
+    mv_scale(scroll);
+  });
+
+});
+
+/*----------------------------------
+ハンバーガーメニュー
+------------------------------------*/
+function hamburger() {
+  $('.hamburger').toggleClass('active');
+
+  if ($('.hamburger').hasClass('active')) {
+    $('#navi').addClass('active');
+  } else {
+    $('#navi').removeClass('active');
+  }
+}
+/*------------------------------------
+mainvisual
+--------------------------------------*/
+function mv_scale(scroll) {
+  if (window.innerWidth > 900) {
+    $('#mainvisual img').css({
+      'width': 100 / 3 + scroll / 10 + '%'
+    });
+  } else {
+    $('#mainvisual img').css({
+      'width': 100 - scroll / 10 + '%'
+    });
+  }
+}
